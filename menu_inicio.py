2272dc2b272a8c51881e784e4d31d3ca5950b975
MariaJoMarin <mariajomarin1312@gmail.com>
2022-03-29 20:08:21 -0600

    Create menu_inicio.py

---
+++ menu_inicio.py
@@ -0,0 +1,159 @@
+import random
+randList=[]
+
+credencialesDoctores = [[113409787,"Angelica Perez","AnP104"],[112652354,"Roberto Bermudez","Bz190955"],[119336712,"Evangelina Barcelo","435Be88"],[1104504382,"Luis Lopez","lL437636"]]
+credencialesPacientes = [[112345678,"Pedro Sanchez","Pedr0"]]
+cedulaLogIn = 0
+contraseniaLogIn = ""
+intentos = 3
+
+expedientesPacientes = [[112345678,"Pedro Sanchez",32]]
+cedula = 0 
+edad = 0
+nombreCompleto = ""
+
+citasMensuales={}
+for i in range(0,30):
+    for k in range(0,10):
+        a=random.randint(0,3)
+        randList.append(a)
+    citasMensuales[i+1] = [[False,"7:00 am",credencialesDoctores[randList[0]][1],"Paciente"],[False,"8:00 am",credencialesDoctores[randList[1]][1],"Paciente"],[False,"9:00 am",credencialesDoctores[randList[2]][1],"Paciente"],[False,"10:00 am",credencialesDoctores[randList[3]][1],"Paciente"],[False,"11:00 am",credencialesDoctores[randList[4]][1],"Paciente"],[False,"12:00 pm",credencialesDoctores[randList[5]][1],"Paciente"],[False,"1:00 pm",credencialesDoctores[randList[6]][1],"Paciente"],[False,"2:00 pm",credencialesDoctores[randList[7]][1],"Paciente"],[False,"3:00 pm",credencialesDoctores[randList[8]][1],"Paciente"],[False,"4:00 pm",credencialesDoctores[randList[9]][1],"Paciente"]]
+
+
+from tkinter import *
+
+MenuInicio=Tk()
+MenuInicio.title("Menu de Inicio")
+MenuInicio.geometry("800x550")
+MenuInicio.resizable(0,0)
+
+Label(MenuInicio,text="Bienvenido al inicio de sesión del hospital",font=("Arial Bold",16)).place(x=190,y=55)
+
+Label(MenuInicio,text="Por favor ingresar lo siguiente",font=("Arial",14)).place(x=255,y=150)
+
+Label(MenuInicio,text="Ingrese su número de cédula",font=(16)).place(x=25,y=250)
+cedula=Entry(MenuInicio)
+cedula.place(x=250,y=255)
+
+Label(MenuInicio,text="Digite su contraseña",font=(16)).place(x=460,y=250)
+contraseña=Entry(MenuInicio)
+contraseña.place(x=630,y=255)
+
+etiquta2=Label(MenuInicio)
+etiquta2.place(x=660,y=355)
+etiquta3=Label(MenuInicio)
+etiquta3.place(x=680,y=375)
+
+
+def presionar():
+    for i in range(len(credencialesDoctores)):
+        if cedulaLogIn in credencialesDoctores[i] and contraseniaLogIn in credencialesDoctores[i]:
+            #os.system("cls")
+            #Menú para doctores
+            while opcion == 0:
+                opcion=int(input("Bienvenido doctor que desea hacer: \n 1.Crear un expediente \n 2.Consultar un expediente \n 3.Modificar un expediente \n 4.Cerrar sesión \n"))
+
+                while opcion == 1:
+                    cedula = int(input("Ingrese el numero de cedula del paciente para crear el expediente "))
+                    nombreCompleto = input("Ingrese el nombre completo del paciente ")
+                    nombreCompleto = nombreCompleto.title()
+                    nombreCompleto = nombreCompleto.rstrip()
+                    nombreCompleto = nombreCompleto.lstrip()
+                    edad = int(input("Ingrese la edad del paciente "))
+
+                    expedientesPacientes.append([cedula, nombreCompleto, edad])
+
+                    print(expedientesPacientes)
+
+                    opcion = int(input("¿Desea crear otro expediente? \n 1.Si \n 2.No \n"))
+                    if opcion != 1:
+                        opcion = 0
+                    #os.system("cls")
+
+                if opcion == 2:
+                    cedula = int(input("Ingrese el numero de cedula del paciente "))
+                    for i in range(len(expedientesPacientes)):
+                        for k in range(len(expedientesPacientes[i])):
+                            if expedientesPacientes[i][k] == cedula:
+                                print(expedientesPacientes[i])
+                                opcion = int(input("Desea modificar el expediente? \n 1.Si \n 2.No \n"))
+                                if opcion == 1:
+                                    #os.system("cls")
+                                    while opcion == 1:
+                                         opcion = int(input("Que desea modificar del expediente \n 1.Agregar padecimiento \n 2.Agregar receta \n 3.Cambiar altura o peso \n"))
+                                         if opcion == 1:
+                                            expedientesPacientes[i].append(input())
+                                         elif opcion == 2:
+                                            expedientesPacientes[i].append(input())
+                                         else:
+                                            opcion = int(input("Que desea cambiar \n 1.Altura \n 2.Peso \n"))
+                                            if opcion == 1:
+                                               try: 
+                                                    expedientesPacientes[i][3] = float(input("Digite la altura "))
+                                               except:
+                                                    expedientesPacientes[i].append(float(input("Digite la altura ")))
+                                            else:
+                                                try: 
+                                                    expedientesPacientes[i][4] = float(input("Digite el peso "))
+                                                except:
+                                                    expedientesPacientes[i].append(float(input("Digite el peso ")))
+                                         print(expedientesPacientes[i])
+                                         opcion = int(input("Desea modificar más el expediente? \n 1.Si \n 2.No \n"))
+                                         if opcion != 1:
+                                            opcion = 0 
+                                            #os.system("cls")
+                                else:
+                                    opcion = 0 
+                                    #os.system("cls")
+
+                if opcion == 3:
+                    cedula = int(input("Ingrese el numero de cedula del paciente "))
+                    for i in range(len(expedientesPacientes)):
+                        for k in range(len(expedientesPacientes[i])):
+                            if expedientesPacientes[i][k] == cedula:
+                                print(expedientesPacientes[i])
+                                opcion = 1
+                                while opcion == 1:
+                                    #os.system("cls")
+                                    opcion = int(input("Que desea modificar del expediente \n 1.Agregar padecimiento \n 2.Agregar receta \n 3.Cambiar altura o peso \n"))
+                                    if opcion == 1:
+                                        expedientesPacientes[i].append(input("Ingrese el padecimiento "))
+                                    elif opcion == 2:
+                                        expedientesPacientes[i].append(input("Ingrese la receta "))
+                                    else:
+                                        opcion = int(input("Que desea cambiar \n 1.Altura \n 2.Peso \n"))
+                                        if opcion == 1:
+                                            try: 
+                                                expedientesPacientes[i][3] = float(input("Digite la altura "))
+                                            except:
+                                                expedientesPacientes[i].append(float(input("Digite la altura ")))
+                                        else:
+                                            try: 
+                                                expedientesPacientes[i][4] = float(input("Digite el peso "))
+                                            except:
+                                                expedientesPacientes[i].append(float(input("Digite el peso ")))
+                                    opcion = int(input("Desea modificar más el expediente? \n 1.Si \n 2.No \n"))
+                                    if opcion != 1:
+                                        opcion = 0 
+                                        #os.system("cls")
+                if opcion == 4:
+                    break
+        
+    """cedulalogIn=cedula.get()
+    contraseñalogIn=contraseña.get()
+    if cedulalogIn == "" and contraseñalogIn == "":
+        print("Una o ambas estan vacias")
+    else:
+        etiquta2.config(text=cedulalogIn)
+        etiquta3.config(text=co)
+        print("Entra a doctores o pacientes")"""
+
+
+
+boton=Button(MenuInicio,text="Haz click aquí",command=presionar).place(x=360,y=310)
+
+Label(MenuInicio,text="*Por favor darle al boton una vez haya terminado",font=("Arial Bold",14)).place(x=190,y=350)
+
+MenuInicio.mainloop()
+
+
